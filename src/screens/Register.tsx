d2dcf7b9df3016899829763af3e9e1ec582aac8b
Anuj Pudasaini <pudasainianuze@gmail.com>
2022-03-30 23:38:41 +0545

    Add api call in user registration

--- src/screens/Register.tsx
+++ src/screens/Register.tsx
@@ -1,23 +1,49 @@
 import React, {useState} from 'react';
 import {StyleSheet, View} from 'react-native';
+import {Text} from 'react-native-paper';
 import {Button, TextInput} from '../components';
+import {theme} from '../core/theme';
 import socket from '../service/socket';
 import {userService} from '../service/store';
 
 export const RegisterScreen = () => {
   const [username, setUsername] = useState('');
+  const [errorMessage, setErrorMessage] = useState('');
 
-  const onSubmit = () => {
+  const onSubmit = async () => {
     if (!username) {
       return;
     }
-    socket.auth = {username};
-    socket.connect();
-    userService.loginUser();
+
+    try {
+      let res = await fetch('http://192.168.1.80:5000/api/user', {
+        headers: {
+          'Content-type': 'application/json; charset=UTF-8',
+        },
+        method: 'POST',
+        body: JSON.stringify({username}),
+      });
+
+      if (res.status === 200) {
+        const {
+          data: {_id: userId},
+        } = await res.json();
+
+        socket.auth = {userId};
+        socket.connect();
+        userService.loginUser();
+      } else {
+        let errorResponse = await res.json();
+        setErrorMessage(errorResponse.message);
+      }
+    } catch (error) {
+      console.log(error);
+    }
   };
 
   return (
     <View style={styles.container}>
+      <Text style={styles.error}>{errorMessage}</Text>
       <TextInput
         label="Username"
         returnKeyType="next"
@@ -40,5 +66,8 @@ const styles = StyleSheet.create({
     flexDirection: 'column',
     justifyContent: 'center',
     alignSelf: 'center',
+  },
+  error: {
+    color: theme.colors.error,
   },
 });
